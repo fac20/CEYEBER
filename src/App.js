e09de4a8b98df28ad84ab1d31ac5fd077a19d4f2
RihardsJ <rihards.jukna@gmail.com>
2020-10-19 14:37:06 +0100

    add ornitron and aldrich font to index file, changed background colour Co-authored-by: My Hoa Tien <myhoatien@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { Title } from "./components/Text.js";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LandingPage from './pages/landingPage';
 
 function App() {
   return (
     <div className="App">
-      <Title />
+      <LandingPage />
     </div>
   );
 }
