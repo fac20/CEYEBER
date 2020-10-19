b27db39741a3189330e55af172b5cee729e383b9
Jessica Deacon <j.r.deacon@hotmail.co.uk>
2020-10-19 12:57:49 +0100

    Adds router library and pages to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { Title } from "./components/Text.js";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Title } from './components/Text.js';
+import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Title />
-    </div>
+    <BrowserRouter>
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Title />
+          </Route>
+          <Route path="/profile" exact></Route>
+          <Route path="/training-manual" exact></Route>
+          <Route path="/first-case-intro" exact></Route>
+          <Route path="/first-case-task" exact></Route>
+          <Route path="/try-again" exact></Route>
+          <Route path="/success" exact></Route>
+          <Route path="/badge" exact></Route>
+          <Route path="/second-case-intro" exact></Route>
+          <Route path="/second-case-task" exact></Route>
+          <Route path="/third-case-intro" exact></Route>
+          <Route path="/third-case-task" exact></Route>
+          <Route path="/game-over" exact></Route>
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
