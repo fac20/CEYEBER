15045a718029e057278b42b4447bd3af307b80c5
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-10-21 13:13:33 +0100

    Adds notes for what's next  Co-authored-by: Jessica <j.r.deacon@hotmail.co.uk>

--- src/pages/thirdCasePage.js
+++ src/pages/thirdCasePage.js
@@ -7,8 +7,22 @@ import {
   IgnoreButton,
   WrongButton
 } from './../components/AnswerButtons';
+//import {Text} from './..components/Text'
 
 //make whichever buttons have been clicked appear on the screen
+//IDEA:
+// create state of answer array with empty array here
+// answer buttons have access to array and setarray
+// each time the button is clicked, add its label to the answer array
+// display answer array on the screen in a text component
+//started: created state on answerpage level, imported text component
+//next: give props to answer buttons on thier page and pass them down here
+//display current state of answer array on page
+
+//OR:could this be a useEffect?
+//every time the points variable changes a button must have been clicked
+//add the label of the button click to a text component on the page
+
 //make next page be a badge if you get a certain number of points!
 
 const ThirdCasePage = ({ points, setPoints, alias }) => {
@@ -19,6 +33,7 @@ const ThirdCasePage = ({ points, setPoints, alias }) => {
     Quick, make your password stronger!`;
 
   const [timeLeft, setTimeLeft] = React.useState(30);
+  //const [answerArray, setAnswerArray] = React.useState([]);
 
   return (
     <ThemeProvider theme={fieldPageTheme}>
@@ -29,6 +44,7 @@ const ThirdCasePage = ({ points, setPoints, alias }) => {
         setTimeLeft={setTimeLeft}
         nextPage={'/try-again'}
       />
+
       <CorrectButton points={points} setPoints={setPoints} label="!!" />
       <IgnoreButton points={points} setPoints={setPoints} label="dog" />
       <CorrectButton points={points} setPoints={setPoints} label="01" />
