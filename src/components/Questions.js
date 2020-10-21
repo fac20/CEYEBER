7af8d9c872a673eaeb701f06e8f94152a45736ba
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-10-21 12:18:57 +0100

    Roughs out plan for third q

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { H1, Text } from './Text';
 import { Img } from './Image';
 
@@ -11,4 +10,17 @@ export const ImageQuestion = ({ title, img, question }) => {
       <Text>{question}</Text>
     </>
   );
+};
+
+//timed question
+//progress bar of time ticking down
+//multiple choice options to add to password- numbers, captial letters, punctuation, their alias,
+//cat, dog, bird
+//adding something that makes your password stronger gives 2 points
+//adding something that makes it weaker takes points away
+//if points is high when timer runs out, you win badge
+//if low, try again
+
+export const TimedQuestion = ({}) => {
+  return <></>;
 };
