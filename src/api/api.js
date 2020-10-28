c2616189c35d92261f949069d03b2d0983713f0f
RihardsJ <rihards.jukna@gmail.com>
2020-10-28 14:50:40 +0000

    sending data of user alias, name and location - need to adjust timestamp later

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,7 @@
-function request(url, options) {
-  return fetch(url, options).then(response => {
+const url = process.env.REACT_APP_DB_URL;
+
+function request(path, options) {
+  return fetch(url + path, options).then(response => {
     if (!response.ok) {
       const error = new Error('Error!');
       error.status = response.status;
@@ -11,9 +13,14 @@ function request(url, options) {
 }
 
 function signUp(alias, age, location) {
-  return request('https://ceyeber.herokuapp.com/users', {
+  return request('user', {
     method: 'POST',
-    body: JSON.stringify({ alias: alias, age: age, location }),
+    body: JSON.stringify({
+      alias: alias,
+      age: age,
+      location: location,
+      created_at: '2020-10-28 14:33:57'
+    }),
     headers: {
       'content-type': 'application/json'
     }
